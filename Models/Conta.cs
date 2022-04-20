9e554e34acd490dc42b52d566e5185d8fe25f965
Dias <higordiaszuqueto@gmail.com>
2022-04-20 09:49:06 -0300

    CliquedinSeguir

---
+++ Models/Conta.cs
@@ -0,0 +1,11 @@
+﻿using Instagram;
+using CliquedinAPI.Models.Conta;
+
+namespace CliquedinSeguir.Models.Contas
+{
+    public class BotAccounts
+    {
+        public Insta insta { get; set; }
+        public ContaCliquedin conta { get; set; }
+    }
+}
